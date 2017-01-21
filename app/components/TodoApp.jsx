32bee2c6b9f093a2ae4d235d45f95a9c8cb90599
Dustin Morris <dwmorris.biz@outlook.com>
2017-01-21 15:29:32 -0600

    added AddTodo component and basic wire up functionality

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
 
 var TodoApp = React.createClass({
 	getInitialState: function () {
@@ -28,11 +29,15 @@ var TodoApp = React.createClass({
 			]
 		};
 	},
+	handleAddTodo: function (text) {
+		alert('new todo is: ' + text);
+	},
 	render: function () {
 		var {todos} = this.state;
 		return (
 			<div>
 				<TodoList todos={todos}/>
+				<AddTodo onAddTodo={this.handleAddTodo}/>
 			</div>
 		)
 	}
